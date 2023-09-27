09511b31bae58cd0b67881b9e39b2c01f7f945e1
Deividas Juska <deividas.juska@kcl.ac.uk>
2023-09-27 15:37:45 +0100

    Added function that returns list of prime numbers dependant on given size

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    count = 1
+    prime = False
+    if (number_of_primes > 0):
+        list.append(2)
+        while(len(list) < number_of_primes):
+            count += 1
+            for num2 in range(2,count):
+                if (count % num2 == 0):
+                    prime = False
+                    break
+                else:
+                    prime = True  
+            if(prime):
+                list.append(count)
+        return list   
+    else:
+        raise ValueError("Inputted value should be greater than 0")
